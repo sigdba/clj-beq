4a0dda74262c52a0f35dae016bafd60d866a4077
Dan Boitnott <dboitnot@gmail.com>
2020-05-15 16:09:07 -0500

    Made process-events higher-order

--- src/com/sigcorp/clj_beq/process.clj
+++ src/com/sigcorp/clj_beq/process.clj
@@ -2,21 +2,15 @@
   (:require [com.sigcorp.clj_beq.events :as e]
             [clojure.tools.logging :as log]))
 
-(defn process-events!
-  "claim, handle, and finalize a single batch of events"
-  [db opts system-code event-code handler-fn finalizer]
-  (let [{:keys [final-user] :or {final-user "clj-beq"}} opts
-        events (e/claim-events! db opts system-code event-code)]
-    (log/debugf "Claimed %d events" (count events))
-    (loop [remaining-events events]
-      (let [event (first remaining-events)]
-        (when-let [seqno (if event (biginteger (:seqno event)))]
-          (log/debugf "Processing event: %s" event)
-          (let [res (handler-fn event)]
-            (log/debugf "Handler (%s) returned '%s' for event #%d" handler-fn res seqno)
-            (log/debugf "Finalizing event #%d with %s" seqno finalizer)
-            (finalizer db final-user event res))
-          (recur (rest remaining-events)))))))
+(defn process-events [claim-fn handler-fn finalize-fn]
+  (->> (claim-fn)
+       (map (fn [event] [event (handler-fn event)]))
+       (map #(apply finalize-fn %))
+       doall))
+
+(defn db-update-finalizer [db final-user]
+  (fn [event status]
+    (e/update-event-status! db final-user status :seqno (:seqno event))))
 
 (def CLAIMED-EVENTS [{:user-id "e3a148ad7b96842860200dd25acd48",
                       :activity-date #inst"2020-05-15T19:18:24.000000000-00:00",
@@ -26,17 +20,10 @@
                       :eqts-code "CLJ",
                       :data {}}])
 
-(let [claim-fn (constantly CLAIMED-EVENTS)
-      handler-fn (fn [event] (log/infof "Handling an event: %s" event) "2")
-      finalize-fn (fn [event status] (log/debugf "Finalizing event %s, status: %s" (:seqno event) status))]
-  (->> (claim-fn)
-       (map (fn [event] [event (handler-fn event)]))
-       (map #(apply finalize-fn %))
-       doall))
+#_(process-events (constantly CLAIMED-EVENTS)
+                (fn [event] (log/infof "Handling an event: %s" event) "2")
+                (fn [event status] (log/infof "Finalizing event %s with status %s" (:seqno event) status)))
 
-#_(process-events! com.sigcorp.clj_beq.db/DB
-                 {}
-                 "CLJ"
-                 nil
-                 (fn [event] (log/infof "Handling an event: %s" event) "2")
-                 (fn [db user-id event status] (e/update-event-status! db user-id status :seqno (:seqno event))))
+#_(process-events #(e/claim-events! com.sigcorp.clj_beq.db/DB {} "CLJ" nil)
+                (fn [event] (log/infof "Handling an event: %s" event) "2")
+                (db-update-finalizer com.sigcorp.clj_beq.db/DB "beq-clj"))
