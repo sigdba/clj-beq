9811a9c815afaf0cbdda01a38c330ae09ab3fb4a
Dan Boitnott <dboitnot@gmail.com>
2020-05-15 13:14:41 -0500

    Whoops. Moved REPL code to function.

--- src/com/sigcorp/clj_beq/process.clj
+++ src/com/sigcorp/clj_beq/process.clj
@@ -2,18 +2,10 @@
   (:require [com.sigcorp.clj_beq.events :as e]
             [clojure.tools.logging :as log]))
 
-(defn- pretend-handler [event]
-  (log/infof "Handling an event: %s" event)
-  "2")
-
-(let [db com.sigcorp.clj_beq.db/DB
-      in-opts {}
-      system-code "CLJ"
-      event-code nil
-      handler-fn pretend-handler
-      finalizer (fn [db user-id event status]
-                  (e/update-event-status! db user-id status :seqno (:seqno event)))]
-  (let [{:keys [final-user] :or {final-user "clj-beq"} :as opts} in-opts
+(defn process-events!
+  "claim, handle, and finalize a single batch of events"
+  [db opts system-code event-code handler-fn finalizer]
+  (let [{:keys [final-user] :or {final-user "clj-beq"}} opts
         events (e/claim-events! db opts system-code event-code)]
     (log/debugf "Claimed %d events" (count events))
     (loop [remaining-events events]
@@ -25,3 +17,10 @@
             (log/debugf "Finalizing event #%d with %s" seqno finalizer)
             (finalizer db final-user event res))
           (recur (rest remaining-events)))))))
+
+#_(process-events! com.sigcorp.clj_beq.db/DB
+                 {}
+                 "CLJ"
+                 nil
+                 (fn [event] (log/infof "Handling an event: %s" event) "2")
+                 (fn [db user-id event status] (e/update-event-status! db user-id status :seqno (:seqno event))))
