ffacde632941a9420a06908354e8c9e16986d4e2
Dan Boitnott <dboitnot@gmail.com>
2020-06-02 12:52:29 -0500

    Started on GitHub workflow

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject com.sigcorp/clj-beq "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Clojure library and utility for managing the Banner event queue"
+  :url "https://github.com/sigdba/clj-beq"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
