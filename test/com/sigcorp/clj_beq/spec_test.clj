2ab45e19ddeb92d0c4a41d85119000ff47c4aeee
Dan Boitnott <dboitnot@gmail.com>
2020-08-18 13:35:06 -0500

    Added some comments

--- test/com/sigcorp/clj_beq/spec_test.clj
+++ test/com/sigcorp/clj_beq/spec_test.clj
@@ -7,11 +7,11 @@
 
 (deftest exercise-specs
   (let [spec-ns "com.sigcorp.clj-beq.spec"]
-    (->> (s/registry)
-         (filter (fn [[k _]] (= spec-ns (namespace k))))
-         (filter (fn [[_ v]] (s/spec? v)))
-         (map first)
-         (map (fn [k]
+    (->> (s/registry)                                       ; Start with the full spec registry.
+         (filter (fn [[k _]] (= spec-ns (namespace k))))    ; Filter it down to our namespace.
+         (filter (fn [[_ v]] (s/spec? v)))                  ; Keep just the specs.
+         (map first)                                        ; Extract the name of the spec.
+         (map (fn [k]                                       ; Exercise the spec.
                 (testing
                   (format "exercising %s" k)
                   (s/exercise k))))
