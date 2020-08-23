9df359aae1608e221dbbca77f014fbccd51e702c
Dan Boitnott <dboitnot@gmail.com>
2020-08-23 12:11:26 -0500

    DB password is now elided from dump output

--- src/com/sigcorp/clj_beq/steps/dump.clj
+++ src/com/sigcorp/clj_beq/steps/dump.clj
@@ -4,11 +4,21 @@
 
 (def RULE (->> (repeat 80 "-") (apply str)))
 
+(def ELIDES [[:db :password]])
+
+(defn- elide
+  [event]
+  (loop [elides ELIDES m event]
+    (let [[e & r] elides
+          new-e (if (get-in m e) (assoc-in m e :elided) e)]
+      (if r (recur r new-e)
+            new-e))))
+
 (defn dump-step
   "returns a step function which dumps the event structure to the log"
   [spec]
   (let [{:keys [label]} spec
         label-s (if label (str " " label) "")]
     (fn [event]
-      (log/infof "DUMP %s\n%s\n%s%s" label-s RULE (with-out-str (pprint event)) RULE)
+      (log/infof "DUMP %s\n%s\n%s%s" label-s RULE (with-out-str (-> event elide pprint)) RULE)
       {:step-status :success})))
