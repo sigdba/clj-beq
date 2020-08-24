8af3b7fc74982c188bb69d3e495c74fae6d0d27d
Dan Boitnott <dboitnot@gmail.com>
2020-08-24 09:38:45 -0500

    Moved dump-var into util

--- src/com/sigcorp/clj_beq/steps/dump.clj
+++ src/com/sigcorp/clj_beq/steps/dump.clj
@@ -1,24 +1,11 @@
 (ns com.sigcorp.clj-beq.steps.dump
-  (:require [taoensso.timbre :as log]
-            [clojure.pprint :refer [pprint]]))
-
-(def RULE (->> (repeat 80 "-") (apply str)))
-
-(def ELIDES [[:db :password]])
-
-(defn- elide
-  [event]
-  (loop [elides ELIDES m event]
-    (let [[e & r] elides
-          new-e (if (get-in m e) (assoc-in m e :elided) e)]
-      (if r (recur r new-e)
-            new-e))))
+  (:require [com.sigcorp.clj-beq.util :refer [dump-var]]))
 
 (defn dump-step
   "returns a step function which dumps the event structure to the log"
   [spec]
-  (let [{:keys [label]} spec
-        label-s (if label (str " " label) "")]
+  (let [{:keys [label] :or {label "DUMP"}} spec
+        level :debug]
     (fn [event]
-      (log/infof "DUMP %s\n%s\n%s%s" label-s RULE (with-out-str (-> event elide pprint)) RULE)
+      (dump-var level label event)
       {:step-status :success})))
